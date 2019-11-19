ebad121073929892d8ebf979fb16f395e05cff37
ijuarezb <ivanjuarez@uchicago.edu>
2019-11-19 00:07:07 -0500

    BitManipulation: Edit Folder

---
+++ 03_BitManipulation/divide_integers.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import sys
+
+# Divide Integers
+# https://www.interviewbit.com/problems/divide-integers/
+#
+# Divide two integers without using multiplication, division and mod operator.
+#
+# Return the floor of the result of the division.
+#
+# Example:
+#
+# 5 / 2 = 2
+#
+# Also, consider if there can be overflow cases. For overflow case, return INT_MAX.
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @return an integer
+    def divide(self, A, B):
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.divide(1, 2))
