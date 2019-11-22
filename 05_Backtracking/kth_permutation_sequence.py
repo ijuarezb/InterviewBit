67293bd70ec6570d349149a945ea59ac1040c448
ijuarezb <ivanjuarez@uchicago.edu>
2019-11-22 13:53:46 -0500

    BackTracking: Kth Permutation Sequence

--- 05_Backtracking/kth_permutation_sequence.py
+++ 05_Backtracking/kth_permutation_sequence.py
@@ -100,9 +100,78 @@ class Solution:
         return s
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Kth item in permutations of an array of 1 to n integers.  Successful submission! Nice approach!!!
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    def getPermutation(self, A, B):
+        return self.helper([x+1 for x in range(A)], A, B )
+        
+    def helper(self, nums, A, B):
+        if A == 1:
+            return str(nums[0])
+        i = (B-1)//math.factorial(A-1)
+        return str(nums[i]) + self.helper(nums[:i]+nums[i+1:], A - 1, (B-1)%math.factorial(A-1)+1)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 if __name__ == '__main__':
     s = Solution()
     print(s.permute([1, 2, 3]))
     print(s.k_permutation(3, 4))
     print(s.getPermutation(8, 82))
     print(s.k_permutation(8, 82))
+
+
+# class Solution:
+#     # @param A : integer
+#     # @param B : integer
+#     # @return a strings
+#     def getPermutation(self, n, k):
+#         d = {0: 1}
+        
+#         for i in range(1,n+1):
+#             d[i] = d[i-1]*i
+#         #print d
+#         l = range(1,n+1)
+#         s = ""
+#         while k:
+#             i = k/d[len(l)-1]
+#             #print k/d[len(l)-1], i
+#             if i == len(l):
+#                 s += str(l[-1])
+#                 k -= (i)*d[len(l)-1]
+#                 l.pop(-1)
+#             else:
+                
+#                 k -= (i)*d[len(l)-1]
+#                 if not k:
+#                     s += str(l[i-1])
+#                     l.pop(i-1)
+#                 else:
+#                     s += str(l[i])
+#                     l.pop(i)
+                
+#             #print s
+        
+#         if l:
+#             s += ''.join(map(str,l[::-1]))
+#         return s
+
+# >>> Editorial <<<
+# from math import factorial as fact
+# class Solution:
+#     # @param A : integer
+#     # @param B : integer
+#     # @return a strings
+#     def getPermutation(self, A, B):
+#         digits = [str(i) for i in range(1,A+1)]
+#         return self.recurse(digits, B-1)
+#        
+#     def recurse(self, digits, k):
+#         n = len(digits)
+#         if n == 1:
+#             return digits[0]
+#        
+#         di = k/fact(n-1)
+#         k2 = k%fact(n-1)
+#         d = digits[di]
+#         digits = digits[:di] + digits[di+1:]
+#         return d + self.recurse(digits, k2)
