52124c9bc2f0f8f3c69e9045c417fb56f460263c
ijuarezb <ivanjuarez@uchicago.edu>
2019-11-22 13:54:13 -0500

    BackTracking: Combination Sum

--- 05_Backtracking/combination_sum.py
+++ 05_Backtracking/combination_sum.py
@@ -4,16 +4,19 @@ import sys
 # Combination Sum
 # https://www.interviewbit.com/problems/combination-sum/
 #
-# Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+# Given a set of candidate numbers (C) and a target number (T), find all unique combinations in 
+# C where the candidate numbers sums to T.
 #
 # The same repeated number may be chosen from C unlimited number of times.
 #
 #     Note:
 #
 #         All numbers (including target) will be positive integers.
-#         Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+#         Elements in a combination (a1, a2, … , ak) must be in non-descending order. 
+#              (ie, a1 ≤ a2 ≤ … ≤ ak).
 #         The combinations themselves must be sorted in ascending order.
-#         CombinationA > CombinationB iff (a1 > b1) OR (a1 = b1 AND a2 > b2) OR … (a1 = b1 AND a2 = b2 AND … ai = bi AND ai+1 > bi+1)
+#         CombinationA > CombinationB iff (a1 > b1) OR (a1 = b1 AND a2 > b2) OR … 
+#              (a1 = b1 AND a2 = b2 AND … ai = bi AND ai+1 > bi+1)
 #         The solution set must not contain duplicate combinations.
 #
 # Example,
@@ -28,6 +31,8 @@ import sys
 class Solution(object):
 
     def _combinationSum(self, A, B, tmp, sum_tmp, left):
+
+        print(tmp, sum_tmp, left)
 
         if sum_tmp == B:
             return [tmp[:]]
