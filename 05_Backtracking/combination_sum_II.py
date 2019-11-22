5b59b25c1d8bd2bf2c65fbdde0374e1c64ff790a
ijuarezb <ivanjuarez@uchicago.edu>
2019-11-22 14:02:36 -0500

    BackTracking: Combination Sum II

--- 05_Backtracking/combination_sum_II.py
+++ 05_Backtracking/combination_sum_II.py
@@ -5,14 +5,16 @@ import sys
 # Combination Sum II
 # https://www.interviewbit.com/problems/combination-sum-ii/
 #
-# Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+# Given a collection of candidate numbers (C) and a target number (T), find all unique combinations 
+# in C where the candidate numbers sums to T.
 #
 # Each number in C may only be used once in the combination.
 #
 #     Note:
 #
 #         All numbers (including target) will be positive integers.
-#         Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+#         Elements in a combination (a1, a2, … , ak) must be in non-descending order. 
+#              (ie, a1 ≤ a2 ≤ … ≤ ak).
 #         The solution set must not contain duplicate combinations.
 #
 # Example :
