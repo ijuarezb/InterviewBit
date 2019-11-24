1a81dec82aed11640d1f4605017d659443ad005a
ijuarezb <ivanjuarez@uchicago.edu>
2019-11-24 17:22:05 -0500

    Hashing: Fraction

--- 05_Hashing/fraction.py
+++ 05_Hashing/fraction.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 import sys
 
+# Fraction
+# https://www.interviewbit.com/problems/fraction/
+#
 # Given two integers representing the numerator and denominator of a fraction, 
 # return the fraction in string format.
 #
 # If the fractional part is repeating, enclose the repeating part in parentheses.
 #
 # Example 1:
-#
 # Input: numerator = 1, denominator = 2
 # Output: "0.5"
-# Example 2:
 #
+# Example 2:
 # Input: numerator = 2, denominator = 1
 # Output: "2"
-# Example 3:
 #
+# Example 3:
 # Input: numerator = 2, denominator = 3
 # Output: "0.(6)"
 #
