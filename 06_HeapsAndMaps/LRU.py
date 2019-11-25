8f95a69a29b803c5b8a36c842d5ff840646e2a1b
ijuarezb <ivanjuarez@uchicago.edu>
2019-11-24 19:52:32 -0500

    Heap&Maps: LRU Cache

--- 06_HeapsAndMaps/LRU.py
+++ 06_HeapsAndMaps/LRU.py
@@ -7,12 +7,15 @@ import sys
 # Design and implement a data structure for Least Recently Used (LRU) cache. It should
 # support the following operations: get and set.
 #
-# get(key) - Get the value (will always be positive) of the key if the key exists in the cache,
+# 1. get(key) - Get the value (will always be positive) of the key if the key exists in the cache,
 # otherwise return -1.
-# set(key, value) - Set or insert the value if the key is not already present. When the cache
-# reaches its capacity, it should invalidate the least recently used item before inserting the new item.
-# The LRUCache will be initialized with an integer corresponding to its capacity. Capacity indicates
-# the maximum number of unique keys it can hold at a time.
+#
+# 2. set(key, value) - Set or insert the value if the key is not already present. When the cache
+# reaches its capacity, it should invalidate the least recently used item before inserting 
+# the new item.
+#
+# The LRUCache will be initialized with an integer corresponding to its capacity. 
+# Capacity indicates the maximum number of unique keys it can hold at a time.
 #
 # Definition of “least recently used” : An access to an item is defined as a get or a set operation
 # of the item. “Least recently used” item is the one with the oldest access time.
@@ -51,7 +54,7 @@ class LRUCache(object):
     # @param key, an integer
     # @param value, an integer
     # @return nothing
-	def set(set, key, value):
+	def set(self, key, value):
 		if key in self.D:
 			self.D.pop(key)
 
@@ -60,4 +63,5 @@ class LRUCache(object):
 
 		self.D[key] = value
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
