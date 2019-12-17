b6d3503e170e593e503c84222fe8452b549b030b
ijuarezb <ivanjuarez@uchicago.edu>
2019-12-17 12:35:17 -0500

    Graphs: Word Search Board

--- 08_GraphsDSAndAlgs/word_search_board.py
+++ 08_GraphsDSAndAlgs/word_search_board.py
@@ -6,7 +6,9 @@ import sys
 #
 # Given a 2D board and a word, find if the word exists in the grid.
 #
-# The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The cell itself does not count as an adjacent cell.
+# The word can be constructed from letters of sequentially adjacent cell, where "adjacent" 
+# cells are those horizontally or vertically neighboring. The cell itself does not count 
+# as an adjacent cell.
 # The same letter cell may be used more than once.
 #
 # Example :
