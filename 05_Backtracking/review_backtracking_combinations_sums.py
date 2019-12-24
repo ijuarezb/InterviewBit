2532a7eede4f8a238e5fd191e85616ac2ceae648
ijuarezb <ivanjuarez@uchicago.edu>
2019-12-24 12:33:46 -0500

    Review Backtracking Combinations Sums

--- 05_Backtracking/review_backtracking_combinations_sums.py
+++ 05_Backtracking/review_backtracking_combinations_sums.py
@@ -40,10 +40,60 @@ class Solution:
 
 		return ans
 
+# combination_sum.py
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+	def combination_sum(self, A, B):
+		A.sort()
+		return self._combination_sum(A, B, [], 0)
+
+	def _combination_sum(self, nums, B, tmp, left):
+		if sum(tmp) == B: return [tmp[:]]
+		if sum(tmp) > B or left >= len(nums): return []
+		ans = []
+
+		for i in range(left, len(nums)):
+			
+			tmp.append(nums[i])
+			ans.extend(self._combination_sum(nums, B, tmp, i))
+			tmp.pop()
+
+		return ans
+
+# combination_sum_II.py
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+	def combination_sum_II(self, A, B):
+		A.sort()
+		return self._combination_sum_II(A, B, [], 0)
+
+	def _combination_sum_II(self, nums, B, tmp, left):
+		if sum(tmp) == B:
+			return [tmp[:]]
+		if sum(tmp) > B or left >= len(nums):
+			return []
+
+		ans = []
+		for i in range(left, len(nums)):
+
+			if nums[i] in tmp and nums[i] != nums[i-1]:
+				continue
+
+			if i > left and nums[i] == nums[i-1]:
+				continue
+			
+			tmp.append(nums[i])
+			ans.extend(self._combination_sum_II(nums, B, tmp, i+1))
+			tmp.pop()
+
+		return ans
+
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 if __name__ == "__main__":
     s = Solution()
-    #print(s.subsets([1, 2, 3]))
-    print(s.combine(4, 2))
-    print(s.permute([1, 2, 3]))
+    # print(s.subsets([1, 2, 3]))
+    # print(s.combine(4, 2))
+    # print(s.permute([1, 2, 3]))
+    # print(s.combination_sum([1,2,3], 4))
+    # print(s.combination_sum([10,1,2,7,6,5], 8))
+    print(s.combination_sum_II([10,1,2,7,6,1,5], 8))
